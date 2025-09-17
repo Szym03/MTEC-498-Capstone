9c60aec49bcea0027cad5ec17974fa7706eb4e96
Szym03 <123407078+Szym03@users.noreply.github.com>
2025-09-17 00:52:59 -0400

    testing accelerometer with microcontroller

---
+++ Project/testing(week3)/tapdemo/tapdemo.ino
@@ -0,0 +1,55 @@
+// Basic demo for tap/doubletap readings from Adafruit LIS3DH modified by me to work with the built in LED of my mictrocontroller
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_LIS3DH.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_NeoPixel.h>
+
+
+
+
+Adafruit_LIS3DH lis = Adafruit_LIS3DH(&Wire1);//use "&Wire1" when using the stemma qt port connection, different pins for I2C
+Adafruit_NeoPixel pixels(1, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+
+#define CLICKTHRESHHOLD 100
+
+void setup(void) {
+  pixels.begin(); 
+  pixels.setBrightness(20);
+  Serial.begin(9600);
+    Serial.println("Adafruit LIS3DH Tap Test!");
+  
+  if (! lis.begin(0x18)) {   
+    Serial.println("Couldnt start");
+    while (1) yield();
+  }
+  Serial.println("LIS3DH found!");
+  
+  lis.setRange(LIS3DH_RANGE_2_G);
+
+  // 0 = turn off click detection & interrupt
+  // 1 = single click only interrupt output
+  // 2 = double click only interrupt output, detect single click
+  // Adjust threshhold, higher numbers are less sensitive
+  lis.setClick(2, CLICKTHRESHHOLD);
+  delay(100);
+}
+
+
+void loop() {
+  uint8_t click = lis.getClick();
+  if (click == 0) return;
+
+  if (click & 0x20) {
+    Serial.println("double click");
+    pixels.fill(0x000000);
+    pixels.show();
+  } else if (click & 0x10) {
+    Serial.println("single click");
+    pixels.fill(0xFF0000);
+    pixels.show();
+  }
+
+  delay(50);
+}
